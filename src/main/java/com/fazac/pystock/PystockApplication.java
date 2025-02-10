c3eca6cedfa5275c9bcaaa1c05e50c26a69e22c9
fazac <1650031931@qq.com>
2025-02-10 21:26:50 +0800

    spring files

---
+++ src/main/java/com/fazac/pystock/PystockApplication.java
@@ -0,0 +1,13 @@
+package com.fazac.pystock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PystockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PystockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fazac/pystock/PystockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fazac.pystock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PystockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
